09ffd7c5b45efc75c29c07dede79e43f3dfc1e3b
The_Zen_Herself <117924296+ZainabAlayande@users.noreply.github.com>
2023-04-27 18:06:47 +0100

    BiFunction Sampls

---
+++ BiFunctionInterface.java
@@ -0,0 +1,13 @@
+package funcInterface;
+
+import java.util.function.BiFunction;
+
+public class BiFunctionInterface {
+    public static void main(String[] args) {
+        System.out.println(biFunction.apply(30, 20));
+
+    }
+
+    static BiFunction<Integer, Integer, String> biFunction =
+            (inputOne, inputTwo) -> inputOne + inputTwo + " " + " ";
+}
